92fb571fcc1b2bec08980230ff6c7d0d816596e6
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-07-20 14:01:41 +0100

    added token contract

---
+++ truffle/contracts/MyToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Eric Token", "Eric") {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
