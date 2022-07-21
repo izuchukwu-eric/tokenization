7356ba50debb67efc9ae6fe3e2b77f230769c39c
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-07-21 21:14:09 +0100

    added token sale contract

---
+++ truffle/contracts/MyTokenSale.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.6.1;
+
+import "./Crowdsale.sol";
+
+contract MyTokenSale is Crowdsale {
+    constructor(
+            unit256 rate,
+            address payable wallet,
+            IERC20 token
+        )
+            Crowdsale(rate, wallet, toekn)
+            public
+        {
+
+        }    
+
+}
