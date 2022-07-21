f34e3bfd0131ba075641b3dad7794b4469f1c022
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-07-21 23:35:52 +0100

    updated deploy contract

--- truffle/migrations/1_deploy_contract.js
+++ truffle/migrations/1_deploy_contract.js
@@ -1,5 +1,14 @@
 const MyToken = artifacts.require("MyToken");
+const MyTokenSale = artifacts.require("MyTokenSale");
+
 
 module.exports = async function (deployer) {
+  const addr = web3.eth.getAccounts();
+
   await deployer.deploy(MyToken, 1000000);
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+
+  //transfer tokens to the tokenSale contract
+  const instance = await MyToken.deployed();
+  instance.transfer(MyTokenSale.address, 1000000);
 };
