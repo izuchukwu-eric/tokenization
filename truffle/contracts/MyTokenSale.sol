a414612d0e3f7d3a8a9bf3acd14d04a31aebb466
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-07-23 02:58:56 +0100

    added kyc contract to tokensale contract

--- truffle/contracts/MyTokenSale.sol
+++ truffle/contracts/MyTokenSale.sol
@@ -3,15 +3,23 @@ pragma solidity 0.6.1;
 import "./Crowdsale.sol";
 
 contract MyTokenSale is Crowdsale {
+
+    KycContract kyc;
     constructor(
             uint256 rate,
             address payable wallet,
-            IERC20 token
+            IERC20 token,
+            KycContract _kyc
         )
             Crowdsale(rate, wallet, token)
             public
         {
+            kyc = _kyc;
+        }   
 
-        }    
+        function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view override {
+            super._preValidatePurchase(beneficiary, weiAmount);
+            require(kyc.KycCompleted(msg.sender), "KYC not completed, purchase not allowed");
+        }  
 
 }
