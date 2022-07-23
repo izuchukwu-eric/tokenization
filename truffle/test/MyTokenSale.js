83d0904234b0d074355a683c099cd4e10e364ac6
Izuchukwu Onukwube <57435925+izuchukwu-eric@users.noreply.github.com>
2022-07-23 01:20:14 +0100

    added test for buy tokens

--- truffle/test/MyTokenSale.js
+++ truffle/test/MyTokenSale.js
@@ -14,8 +14,27 @@ contract("TokenSale Test", function (accounts) {
 
     it("should not have any token in my deployerAccount", async () => {
         const instance = await Token.deployed();
-        
+
         await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
+    })
+
+    it("all tokens should be in the tokensale contract by default", async () => {
+        let instance =  await Token.deployed();
+
+        let balanceOfTokenSaleContract = await instance.balanceOf(TokenSale.address);
+        let totalSupply = await instance.totalSupply();
+
+        await expect(balanceOfTokenSaleContract).to.be.a.bignumber.equal(totalSupply);
+
+    })
+
+    it("should be able to buy tokens", async () => {
+        const tokenInstance = await Token.deployed();
+        const tokenSaleInstance = await TokenSale.deployed();
+        const balanceBefore = await tokenInstance.balanceOf(deployerAccount);
+
+        await expect(tokenSaleInstance.sendTransaction({to: deployerAccount, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+        await expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceBefore.add(new BN(1)));
     })
 
 })   
